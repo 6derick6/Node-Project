f46ecf3b34167f2edbab49cb7faa0211d7220e3d
6derick6 <6derick6@gmail.com>
2023-02-27 13:07:42 -0300

    Creating initial routes, configuring views and static folder!

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
-app.listen(5000,()=>{
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+app.use('/public', express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, '/views'));
+
+app.get('/',(req,res)=>{
+
+    res.render('index',{nome:'Dérick'});
+
+});
+
+app.listen(4000,()=>{
     console.log('servidor rodando!');
 })
