18ecf322324a4088b36905252f0ea0fba280e7c0
6derick6 <6derick6@gmail.com>
2023-02-27 21:21:40 -0300

    Desenvolvendo projeto 1/2

--- index.js
+++ index.js
@@ -8,11 +8,22 @@ app.set('view engine', 'html');
 app.use('/public', express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, '/views'));
 
+var tarefas = ['Arrumar a casa','Finalizar curso de JS'];
+
 app.get('/',(req,res)=>{
 
-    res.render('index',{nome:'Dérick'});
+    res.render('index',{tarefasList:tarefas});
 
 });
+
+app.get('/deletar/:id',(req,res)=>{
+    tarefas = tarefas.filter(function(val,index){
+            if(index != req.params.id){
+                return val;
+            }
+    })
+    res.render('index',{tarefasList:tarefas});
+})
 
 app.listen(4000,()=>{
     console.log('servidor rodando!');
